6adcd42147df873c7b725347df944b23a556121f
mine2424 <minewest065524@gmail.com>
2022-02-23 13:06:55 +0900

    feat(plugins): firebaseの初期設定を実装

--- src/common/plugins/firebase.ts
+++ src/common/plugins/firebase.ts
@@ -1,15 +1,20 @@
-import firebase from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getApps, initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.FIREBASE_SENDER_ID,
-};
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+}
 
-firebase.initializeApp(firebaseConfig);
+export const apps = getApps()
 
-export const auth = getAuth();
+if (!apps.length) {
+  initializeApp(firebaseConfig)
+}
+
+export const auth = getAuth()
