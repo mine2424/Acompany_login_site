4068abf1e15fe7b5551f78442d678092f7f43e05
mine2424 <minewest065524@gmail.com>
2022-02-23 13:24:02 +0900

    feat(repository): 新規登録、ログイン、ログアウト、ログイン状態判別機能の実装

--- src/domain/interface/repository/userRepository.ts
+++ src/domain/interface/repository/userRepository.ts
@@ -1,1 +1,21 @@
-export default interface UserRepository {}
+import { UserCredential } from 'firebase/auth'
+
+export default interface UserRepository {
+  listenAuthStatus(): AuthStatus
+  createUserWithEmailAndPassword(
+    email: string,
+    password: string
+  ): Promise<AuthStatus>
+  signOut(): Promise<boolean>
+  signInWithEmailAndPassword(
+    email: string,
+    password: string
+  ): Promise<AuthStatus>
+}
+
+export const enum AuthStatus {
+  NONE,
+  ERROR,
+  LOADING,
+  AUTHENTICATED,
+}
--- src/domain/repository/userRepository.ts
+++ src/domain/repository/userRepository.ts
@@ -1,5 +1,68 @@
-import { User } from "../entity/user"
-import UserRepository from "../interface/repository/userRepository"
+import {
+  createUserWithEmailAndPassword,
+  inMemoryPersistence,
+  setPersistence,
+  signInWithEmailAndPassword,
+  UserCredential,
+} from 'firebase/auth'
+import { User } from '../entity/user'
+import UserRepository from '../interface/repository/userRepository'
+import { AuthStatus } from '../interface/repository/userRepository'
+import { auth } from 'src/common/plugins/firebase'
 
+export default class UserRepositoryImp implements UserRepository {
+  listenAuthStatus(): AuthStatus {
+    const currentUser = auth.currentUser
 
-export default class UserRepositoryImp implements UserRepository {}
+    if (currentUser) {
+      return AuthStatus.AUTHENTICATED
+    }
+
+    return AuthStatus.NONE
+  }
+
+  async createUserWithEmailAndPassword(
+    email: string,
+    password: string
+  ): Promise<AuthStatus> {
+    try {
+      setPersistence(auth, inMemoryPersistence)
+      await createUserWithEmailAndPassword(auth, email, password)
+
+      return AuthStatus.AUTHENTICATED
+    } catch (error: any) {
+      return error.message
+    }
+  }
+
+  async signInWithEmailAndPassword(
+    email: string,
+    password: string
+  ): Promise<AuthStatus> {
+    if (auth.currentUser !== null) {
+      return AuthStatus.ERROR
+    }
+
+    try {
+      setPersistence(auth, inMemoryPersistence)
+
+      await signInWithEmailAndPassword(auth, email, password)
+      return AuthStatus.AUTHENTICATED
+    } catch (e: any) {
+      return e.message
+    }
+  }
+
+  async signOut(): Promise<boolean> {
+    if (auth.currentUser == null) {
+      return true
+    }
+
+    try {
+      await auth.signOut()
+      return true
+    } catch (e: any) {
+      return e.message
+    }
+  }
+}
