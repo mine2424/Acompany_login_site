9b387948b2d23dd72853e261b82d7fcfcc3d90e4
mine2424 <minewest065524@gmail.com>
2022-02-23 17:43:41 +0900

    fix: user entityを使用してログイン機能を実装

--- src/domain/interface/repository/userRepository.ts
+++ src/domain/interface/repository/userRepository.ts
@@ -1,16 +1,11 @@
 import { UserCredential } from 'firebase/auth'
+import { User } from 'src/domain/entity/user'
 
 export default interface UserRepository {
   listenAuthStatus(): AuthStatus
-  createUserWithEmailAndPassword(
-    email: string,
-    password: string
-  ): Promise<AuthStatus>
+  createUserWithEmailAndPassword(user: User): Promise<AuthStatus>
   signOut(): Promise<boolean>
-  signInWithEmailAndPassword(
-    email: string,
-    password: string
-  ): Promise<AuthStatus>
+  signInWithEmailAndPassword(user: User): Promise<AuthStatus>
 }
 
 export const enum AuthStatus {
